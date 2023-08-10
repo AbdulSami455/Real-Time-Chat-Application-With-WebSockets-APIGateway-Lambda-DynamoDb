3136cbaa51580b73c5d24577b67c676435df729c
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-11 04:40:50 +0500

    Connection Function

--- websocketconnection.py
+++ websocketconnection.py
@@ -1,1 +1,28 @@
 
+#These all are Module REquired for Working
+import json
+import boto3
+import os
+
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    connectionId = event['requestContext']['connectionId']
+
+    dynamodb.put_item(
+        TableName=os.environ['WEBSOCKET_TABLE'],
+        Item={'connectionId': {'S': connectionId}}
+    )
+
+    return {}
+dynamodb = boto3.client('dynamodb')
+
+def lambda_handler(event, context):
+    connectionId = event['requestContext']['connectionId']
+
+    dynamodb.put_item(
+        TableName=os.environ['WEBSOCKET_TABLE'],
+        Item={'connectionId': {'S': connectionId}}
+    )
+
+    return {}
